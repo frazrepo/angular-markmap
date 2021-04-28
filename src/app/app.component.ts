ce911640115e2e8db91957ef8ef26f690ce9c04f
fraztech <frazvisuals@gmail.com>
2021-04-28 22:33:17 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  Transformer, fillTemplate,
+} from 'markmap-lib';
+import { Markmap } from 'markmap-view';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-markmap';
+  constructor(private http: HttpClient) {
+    this.doMarkMap();
+  }
+
+  async doMarkMap() {
+    var markdownUrl = '/assets/node.md';
+    var markdown = await this.http.get(markdownUrl, { responseType : 'text'}).toPromise();
+
+    const transformer = new Transformer();
+    const { root, features } = transformer.transform(markdown);
+    
+    Markmap.create('#markmap', null, root);
+  }
 }
---
+++ src/assets/node.md
@@ -0,0 +1,6 @@
+# Titre
+# Titre 3
+# Titre 4
+# Titre 5
+# Titre 6
+# Titre 7
